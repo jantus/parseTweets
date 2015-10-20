848f146039ce8874928aba71add3a6f5e947b1d1
Joakim Antus <joakimantus@gmail.com>
2015-10-20 20:31:50 +0200

    Added script to start and stop server

---
+++ newServer/server_stop.py
@@ -0,0 +1,66 @@
+import sys, os, time
+from novaclient.client import Client
+
+
+config = {'username':os.environ['OS_USERNAME'], 
+          'api_key':os.environ['OS_PASSWORD'],
+          'project_id':os.environ['OS_TENANT_NAME'],
+          'auth_url':os.environ['OS_AUTH_URL'],
+           }
+nc = Client('2',**config)
+KEYPAIRNAME = "openstack-joakim"
+SERVERNAME = "instance-lab3-joakim"
+FLAVOR =  nc.flavors.find(name="m1.medium")
+IMAGE = nc.images.find(name="Ubuntu Server 14.04 LTS (Trusty Tahr)")
+floating_ip = 0
+
+def initialize(): 
+
+    serverList = nc.servers.list(search_opts={'name': SERVERNAME})
+
+    if serverList:
+        server = serverList[0]
+        print "Found server named:", SERVERNAME
+    else:
+
+        try:
+            keypair = nc.keypairs.find(name=KEYPAIRNAME)
+        except:
+            print "Erik was right"
+
+        
+        script = file("userdata.yml")
+        userdata = script.read()
+        server = nc.servers.create(SERVERNAME, IMAGE, FLAVOR, key_name=keypair.name, userdata=userdata)
+        script.close()
+        
+        print "Created server named:", SERVERNAME
+
+    time.sleep(15)
+   
+    floating_ip = nc.floating_ips.create(nc.floating_ip_pools.find(name="ext-net").name)
+    try:
+        server.add_floating_ip(floating_ip)
+        print "Associated ip "+str(floating_ip.ip)+" with instance"
+    except:
+        print "Could not associate ip with instance, terminated with message:",sys.exc_info()[0]
+        print "Current IPs associated with instance", nc.servers.ips(server)
+
+
+
+    return server, floating_ip.ip
+
+def terminate(): 
+    # Terminate all your running instances
+    serverList = nc.servers.list(search_opts={'name': SERVERNAME})
+    if serverList:
+        server = serverList[0]
+        print "Found server named:", SERVERNAME
+    try:
+        nc.servers.delete(server)
+        print "Server terminated"
+        sleep(15)
+    except:
+        print "Server is not definded"
+
+
